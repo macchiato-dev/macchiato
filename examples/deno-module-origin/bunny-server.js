6bc0b67cba6a97fce67683743905a2a316bc476f
Benjamin Atkin <ben@resources.co>
2026-08-03 02:02:45 +0000

    Revert revisioned Bunny module deployment experiment

--- examples/deno-module-origin/bunny-server.js
+++
@@ -1,5 +0,0 @@
-import * as BunnySDK from "@bunny.net/edgescript-sdk";
-import process from "node:process";
-import { createModuleOriginHandler } from "./handler.js";
-
-BunnySDK.net.http.serve(createModuleOriginHandler(process.env));
