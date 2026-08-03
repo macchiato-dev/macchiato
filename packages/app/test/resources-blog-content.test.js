c44ca6dd35ca3c8450b88c33e349e762d8622f56
Benjamin Atkin <ben@resources.co>
2026-08-03 02:36:35 +0000

    Publish WebAssembly sandboxed containers article

---
+++ packages/app/test/resources-blog-content.test.js
@@ -0,0 +1,18 @@
+import assert from "node:assert/strict";
+import test from "node:test";
+import { loadBlogPosts, renderBlogInline } from "../../../examples/resources-site/blog-content.js";
+
+const escapeHtml = (value) => String(value).replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;").replace(/"/g, "&quot;");
+
+test("WebAssembly containers post is complete in English and Spanish", () => {
+  for (const locale of ["en", "es"]) {
+    const post = loadBlogPosts(undefined, locale).find(({ slug }) => slug === "webassembly-based-sandboxed-containers");
+    assert.ok(post);
+    assert.match(post.paragraphs.at(-1), locale === "en" ? /practical middle ground/ : /punto medio práctico/);
+    assert.ok(post.paragraphs.length >= 10);
+  }
+});
+
+test("blog inline rendering supports emphasis without weakening escaping", () => {
+  assert.equal(renderBlogInline("Use *dom-use* with <care>.", escapeHtml), "Use <em>dom-use</em> with &lt;care&gt;.");
+});
